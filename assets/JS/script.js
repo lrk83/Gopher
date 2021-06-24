9fcda9ed771ad677c91aff98b23863c58d7c53cd
Lukas Keel <lrk83@cornell.edu>
2021-06-24 11:37:50 -0500

    added connect modal

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -355,7 +355,7 @@ $('#eventInfo').on("click",".getDirections",function(){
 
 var modalBtn = document.querySelector("#modalBtn");
 var modalBg = document.querySelector("#modalBG");
-var modalClose = document.querySelector(".modal-close");
+var modalClose = document.querySelector("#modalClose");
 
 modalBtn.addEventListener("click",function(){
     modalBg.classList.add("bg-active");
@@ -365,7 +365,18 @@ modalClose.addEventListener("click",function(){
     modalBg.classList.remove("bg-active");
 });
 
+var connectBtn = document.querySelector("#connectBtn");
+var connectBg = document.querySelector("#connectModalBG")
+var connectClose = document.querySelector("#connectClose");
 
+connectBtn.addEventListener("click",function(){
+    console.log("clicked");
+    connectBg.classList.add("bg-active");
+});
+
+connectClose.addEventListener("click",function(){
+    connectBg.classList.remove("bg-active");
+});
 
 // local storage 
 var tasks = [];
